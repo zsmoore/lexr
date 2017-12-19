51e1c6fc8811be544b2c96fbeb8c5619a594d247
zsmoore <zsmoore@zachary-moore.com>
2017-12-19 18:35:45 -0500

    numeric literal complete

--- src/tokenEnum.js
+++ src/tokenEnum.js
@@ -1,21 +1,58 @@
-const character = new RegExp(/(\\t|\\n|\\\\|.)/);
+/*
 
+    Created by Zachary Moore
+    github.com/zsmoore
+    zsmoore@zachary-moore.com
+
+    All token regex productions defined following
+    ECMAScript Language Specification 5.1 Edition / June 2011
+    Standard ECMA-262 
+    Found at: https://www.ecma-international.org/ecma-262
+
+*/
 
 //  Numeric Literal
 const decimalDigit = new RegExp(/([0-9])/);
 const decimalDigits = new RegExp('(' + decimalDigit.source + '+)');
 const nonZeroDigit = new RegExp(/([1-9])/);
 const decimalIntegerLiteral = new RegExp('(0|(' + nonZeroDigit.source + decimalDigits.source + '?))');
+const signedInteger = new RegExp('((\\+|-)?' + decimalDigits.source + ')');
 const exponentIndicator = new RegExp('(e|E)');
-const signedInteger = new RegExp('((\+|-)?' + decimalDigits.source + ')');
 const exponentPart = new RegExp('(' + exponentIndicator.source + signedInteger.source + ')');
-const decimalLiteral = new RegExp('((' + decimalIntegerLiteral.source + '.' 
+const decimalLiteral = new RegExp('((' + decimalIntegerLiteral.source + '\\.' 
     + decimalDigits.source + '?' + exponentPart.source + '?' + ')'
-    + '|(.' + decimalDigits.source + exponentPart.source + '?' + ')'
+    + '|(\\.' + decimalDigits.source + exponentPart.source + '?' + ')'
     + '|(' + decimalIntegerLiteral.source + exponentPart.source + '?))');
 const hexDigit = new RegExp(/([0-9]|[a-f]|[A-F])/);
 const hexIntegerLiteral = new RegExp('((0x|0X)' + hexDigit.source + '+)');
 const numericLiteral = new RegExp('(' + decimalLiteral.source + '|' + hexIntegerLiteral.source + ')');
+const fullNumericLiteral = new RegExp('\\b' + numericLiteral.source + '\\b');
+
+// String Literal
+const lineTerminator = new RegExp();
+const lineTerminatorSequence = new RegExp();
+
+const characterEscapeSequence = new RegExp('()');
+const hexEscapeSequence = new RegExp('()');
+const unicodeEscapeSequence = new RegExp('()');
+
+const escapeSequence = new RegExp('(' + characterEscapeSequence.source +
+    + '|(0(?!' + decimalDigit.source + '))'
+    + '|' + hexEscapeSequence.source +
+    + '|' + unicodeEscapeSequence.source + ')');
+const lineContinuation = new RegExp('(\\' + lineTerminatorSequence.source + ')');
+
+
+const doubleStringCharacter = new RegExp('(([^\\"' + lineTerminator.source + '])|(\\'
+    + escapeSequence.source + ')' 
+    + '|(' + lineContinuation.source + '))');
+const doubleStringCharacters = new RegExp('(' + doubleStringCharacter.source + '+)');
+const singleStringCharacter = new RegExp("(([^\\'" + lineTerminator.source + "])|(\\"
+    + escapeSequence.source + ")"
+    + "|(" + lineContinuation.source + "))");
+const singleStringCharacters = new RegExp('(' + singleStringCharacter.source + '+)');
+const stringLiteral = new RegExp('(("' + doubleStringCharacters.source + '?")|(\''
+    + singleStringCharacters.source + '?\'))');
 
 const tokens = {
 
@@ -70,10 +107,10 @@ const tokens = {
     // Punctuators
     L_BRACE         : /{/,
     R_BRACE         : /}/,
-    L_PAREN         : /(/,
-    R_PAREN         : /)/,
-    L_BRACKET       : /[/,
-    R_BRACKET       : /]/,
+    L_PAREN         : /\(/,
+    R_PAREN         : /\)/,
+    L_BRACKET       : /\[/,
+    R_BRACKET       : /\]/,
     DOT             : /./,
     SEMI_COLON      : /;/,
     COMMA           : /,/,
@@ -85,12 +122,12 @@ const tokens = {
     NOT_EQ          : /!=/,
     STRICT_EQ       : /===/,
     NOT_STRICT_EQ   : /!==/,
-    ADD             : /+/,
+    ADD             : /\+/,
     SUB_OR_NEG      : /-/,
-    MUL             : /[*]/,
+    MUL             : /\*/,
     MOD             : /%/,
-    INC             : /++/,
-    DECR            : /--/.
+    INC             : /\+\+/,
+    DECR            : /--/,
     ZERO_FILL_LEFT  : /<</,
     SIGNED_RIGHT    : />>/,
     ZERO_FILL_RIGHT : />>>/,
@@ -101,12 +138,12 @@ const tokens = {
     BIT_NOT         : /~/,
     AND             : /&&/,
     OR              : /||/,
-    TERN_IF         : /?/,
+    TERN_IF         : /\?/,
     TERN_ELSE       : /:/,
     ASSIGN          : /=/,
-    ADD_ASSIGN      : /+=/,
+    ADD_ASSIGN      : /\+=/,
     SUB_ASSIGN      : /-=/,
-    MUL_ASSIGN      : /[*=]/,
+    MUL_ASSIGN      : /\*=/,
     MOD_ASSIGN      : /%=/,
     SHIFTL_ASSIGN   : /<<=/,
     SHIFTR_ASSIGN   : />>=/,
@@ -121,8 +158,6 @@ const tokens = {
     NULL_LIT        : /null/,
     TRUE_LIT        : /true/,
     FALSE_LIT       : /false/,
-    NUM_LIT         : numericLiteral.source,
-
-
+    NUM_LIT         : fullNumericLiteral.source,
 
 };
