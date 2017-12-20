8ae8f1e8219751c372474ceda82fd917e52d55a0
zsmoore <zsmoore@zachary-moore.com>
2017-12-20 02:11:21 -0500

    identifier fix

--- src/index.js
+++ src/index.js
@@ -5,4 +5,37 @@ let input = ` if ( true ) {
     } else {
     let variable = 123.2
     }`;
-parse(input);
+
+let input2 = `var a = 5;
+var b = 'test Variable';
+var c = 10;
+var d = 500;
+var e = f = g = h = c / d;
+var condition1 = g < b;
+var condition2 = c > d;
+var condition3 = c > a;
+if (condition1) {
+    var a = false;
+} else if (false) {
+    var b = 20;
+} else if (condition2) {
+    var c = 'not executed';
+} else if (condition3) {
+    var d = 'This will be executed';  
+    if (condition1) {
+        var a = false;
+    } else if (false) {
+        var b = 20;
+    } else if (condition2) {
+
+        var c = 'not executed';
+    } else if (condition3) {
+        var d = 'This will be executed';
+    } else {
+        var e = undefined;    
+    }
+} else {
+    var els = 'esle';
+}
+var end = 'end';`;
+parse(input2);
